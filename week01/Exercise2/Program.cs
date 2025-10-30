af6b41783a578d1c2a89f92b21d0478762279e0b
Dianne McMahon <dn2mcmahon@gmail.com>
2025-10-30 15:05:54 -0400

    Starting to write code for Wk01 exercise 2 CSE210 - Grades

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -6,4 +6,24 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
     }
+
+    {
+        Console.WriteLine("What is your grade percentage (without the percentage sign)? ");
+    
+        if (grade >= 90)
+            Console.WriteLine("A");
+        else if (grade >= 80)
+            Console.WriteLine("B");
+        else if (grade >= 70)
+            Console.WriteLine("C");
+        else if (grade >= 60)
+            Console.WriteLine("D");
+        else:
+            Console.WriteLine("F");
+
+        sign = ""
+            last_digit = grade % 10
+    
+    
+    }
 }
