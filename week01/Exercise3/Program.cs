55a2e7abe1114a6b66a5c608562abd7b1551cb5c
Dianne McMahon <dn2mcmahon@gmail.com>
2025-10-31 13:35:13 -0400

    writing code for week 01 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,36 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Console.Write("What is the magic number? ");
+        int magic = int.Parse(Console.ReadLine());
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            
+        }
     }
-
-        int magic = ();
-
-        {
-            Console.Write("Pick a magic number. ");
-        }
-
-        guess = ();
-
-        {
-            Console.WriteLine("Guess a number between 1 and 10". );
-        }
-
-        while (guess != number);
-
-        {
-            Console.WriteLine()
-
-        if guess < number;
-        {
-            Console.WriteLine("Higher");
-        }
-
-        else if guess > number;
-        {
-            Console.WriteLine("Lower");
-        }
-            guess = Console.ReadLine();
-        }
 
     
 }
