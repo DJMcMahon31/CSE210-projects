b133bae5eb1806ba7ddefb9166df5ade878801aa
Dianne McMahon <dn2mcmahon@gmail.com>
2025-10-31 18:38:42 -0400

    added comments

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,38 +7,36 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
 
-        List<int> numbers = new List<int>();
-        int number;
-        do
+        List<int> numbers = new List<int>();  //create a new list.
+        int number;  //assign a variable.
+        do  //do-while loop.
         {
-            Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
-            number = int.Parse(Console.ReadLine()); //18 9 7 0
+            Console.WriteLine("Enter a list of numbers, type 0 when finished. "); //print line for user to input data.
+            number = int.Parse(Console.ReadLine());   //use Parse to change from string to integer.
 
-            if (number != 0)
+            if (number != 0)  //if line to add numbers to list except for the number 0.
             {
-                numbers.Add(number); //18 9 7
+                numbers.Add(number); //add the numbers to the list.
             }
 
-
-
         }
-        while (number != 0);
+        while (number != 0); //while loop to end loop if number entered by user is 0.
 
-        int sum = 0;
-        foreach (var item in numbers)
+        int sum = 0;  //interger variable that equals 0.
+        foreach (var item in numbers)  //foreach loop to 
         {
-            sum += item;  
+            sum += item;  //equation to add the numbers in the list together. this requires a separate block (curly brackets).
         }
 
-        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The sum is {sum}");  //print the sum.
         
-        float average = sum / numbers.Count;
+        float average = sum / numbers.Count;  //equation to find the average.
 
-        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The average is: {average}");  //print the average.
 
-        int maximum = numbers.Max();
+        int maximum = numbers.Max();  //equation to find the largest number in the list.
 
-        Console.WriteLine($"The largest number is: {maximum}");
+        Console.WriteLine($"The largest number is: {maximum}");  //print the largest number in the list.
 
     }
 }
