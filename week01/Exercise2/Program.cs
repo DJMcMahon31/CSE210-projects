60f0a1eab860c60fd870c0e77f0566b3b90163b5
Dianne McMahon <dn2mcmahon@gmail.com>
2025-10-31 18:48:49 -0400

    commented out "Hello World..."

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
 
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise2 Project.");
 
     
         Console.WriteLine("What is your grade percentage (without the percentage sign)? ");
