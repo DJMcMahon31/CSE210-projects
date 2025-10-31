49755ce2ce697083ed1eaee09c7030862df0ede3
Dianne McMahon <dn2mcmahon@gmail.com>
2025-10-31 18:48:05 -0400

    commented out "Hello World..."

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise5 Project.");
     }
+
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    {
+        Console.WriteLine("Please enter your name: ");
+    }
+
 }
