871f18d7c0270ec004b6aa5bbec161e0ff7a94b8
Dianne McMahon <dn2mcmahon@gmail.com>
2025-10-31 18:49:30 -0400

    commented out "Hello World..."

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise3 Project.");
         
         Console.Write("What is the magic number? ");
         int magic = int.Parse(Console.ReadLine());
