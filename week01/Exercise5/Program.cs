f2e4af4be99f1a73e7c79d078acfd8f4d2df759c
Dianne McMahon <dn2mcmahon@gmail.com>
2025-10-31 20:03:29 -0400

    wrote code for week 01 exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -3,16 +3,50 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        // Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
+    //{
+    // Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    //}
 
+    {
+        DisplayMessage();
+
+        string userName = PromptUserName();
+        int favNumber = PromptFavNumber();
+
+        double squareRoot = CalculateSquareRoot(favNumber);
+
+        DisplayResult(userName, squareRoot);
+    }
+        
+    static void DisplayMessage()
     {
         Console.WriteLine("Welcome to the Program!");
-    }
 
+
+    }
+    static string PromptUserName()
     {
-        Console.WriteLine("Please enter your name: ");
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+
+    }
+    static int PromptFavNumber()
+    {
+        Console.Write("Please enter your favourite number: ");
+        int favNumber = int.Parse(Console.ReadLine());
+
+        return favNumber;
     }
 
+    static double CalculateSquareRoot(int favNumber)
+    {
+        return Math.Sqrt(favNumber);
+    }
+
+    static void DisplayResult(string userName, double squareRoot)
+    {
+        Console.WriteLine($"{userName}, the square root of your favourite number is {squareRoot:F2}");
+    }
 }
