513d7db540ba22c802742b122241453bb27e1705
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-06 18:37:56 -0500

    Continued with creating Journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 
 class Program
 {
@@ -10,6 +11,36 @@ class Program
         Journal myJournal = new Journal();
         PromptGenerator promptGen = new PromptGenerator();
         Entry newEntry = new Entry();
-        anEntry.Display();
-    }
+
+        Console.WriteLine("1.New Journal Entry");
+        Console.WriteLine("3.Display the Journal");
+        Console.WriteLine("4.Save the Journal to a file");
+        Console.WriteLine("5.Load the Journal from a file");
+        Console.WriteLine("6.Quit");
+
+       
+        Console.WriteLine("Choose an option number: ");
+        string option = Console.ReadLine();
+
+        if (option == "1")
+        {
+            //string getPromptGenerator = promptGen.GetRandomPrompt();
+            Console.WriteLine(promptGen.GetRandomPrompt());
+        }
+
+        if (option == "2")
+        {
+            myJournal.DisplayAll();
+        }
+
+        if (option == "3")
+        {
+            Console.WriteLine("Save to file option selected.");
+        }
+        
+
+
+
+        
+    }   
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -10,7 +10,7 @@
 
 
 using System;
-
+using System.Collections.Generic;
 public class PromptGenerator
 {
     public List<string> _prompts = new List<string>()
@@ -19,14 +19,20 @@ public class PromptGenerator
         "What are you grateful for today?",
         "Describe a challenge you faced recently.",
         "What is a goal you have for the next week?",
-        "Write about someone who inspires you."
+        "Write about someone who inspires you.",
+        "Who was the most interesting person I interacted with today?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+
     };
 
-    //private Random _random = new Random();
+    private Random _random = new Random();
 
     public string GetRandomPrompt()
     {
         int index = _random.Next(_prompts.Count);
         return _prompts[index];
     }
+
 }
