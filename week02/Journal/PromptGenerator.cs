e745afc6956090a1fd8cf732253d3165c687e821
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-07 12:14:37 -0500

    aded more prompts

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -23,8 +23,13 @@ public class PromptGenerator
         "Who was the most interesting person I interacted with today?",
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
-
+        "If I had one thing I could do over today, what would it be?",
+        "Who was in your thoughts today and how did that make you feel?",
+        "What is a new experience you had recently?",
+        "Were you inspired to do anything today? If so, what?",
+        "What tickled your fancy bone today?",
+        "What was the weather like today and how did it affect your day?",
+        "What would you share with a younger version of yourself?"
     };
 
     private Random _random = new Random();
