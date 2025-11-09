914ff2becbf7ba321be0af2aed90785e822187c1
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-08 21:11:53 -0500

    added three constructors to Program.cs

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,8 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        //Console.WriteLine("Hello World! This is the Fractions Project.");
 
-        
+        //create an instance of the first constructor (1/1)
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"Fraction 1: {fraction1}");
+
+        //create an instance of the second constructor (top/1)
+        Fraction fraction2 = new Fraction(6);
+        Console.WriteLine($"Fraction 2: {fraction2}");
+
+        //create an instance of the third constructor (top/bottom)
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine($"Fraction 3: {fraction3}");
+
+
     }
 }
