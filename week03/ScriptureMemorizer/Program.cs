d6c441b8b9b6369630c7c4bef59d7bfd63a1f572
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-14 12:31:55 -0500

    cleaned up errors in Program.cs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,24 +6,20 @@ class Program
     {
         //Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
 
-        Reference reference = new Reference("Nephi", 3, 6, 7);
+        Reference reference = new Reference("Nephi", "3", "6", "7");
         //string text = Console.ReadLine();
         Scripture scripture = new Scripture (reference, "And behold, the third time they did understand the voice which they heard; and it said unto them: Behold my Beloved Son, in whom I am well pleased, in whom I have glorified my name—hear ye him.");
 
         Console.WriteLine("Here is a scripture to memorize.");
-        Console.WriteLine(scripture.GetDisplayText(0)); //difficulty set to zero shows the whole thing
+        Console.WriteLine(scripture.GetDisplayText()); //shows the whole thing
 
 
-        while
-        {
-            
-
-            Console.WriteLine("Press Enter to continue.");
-            Console.ReadLine();
-        }
+        // Wait for the user to press Enter once to continue
+        Console.WriteLine("Press Enter to continue.");
+        Console.ReadLine();
 
         Console.Clear();
-        Console.WriteLine(scripture.GetDisplayText(5));
+        Console.WriteLine(scripture.GetDisplayText());
 
     
         string text = Console.ReadLine();
