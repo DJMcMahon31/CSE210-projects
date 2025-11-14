f106200c8713416b92628ed22fcda03c907985fb
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-13 20:13:31 -0500

    coding Program.cs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,9 +6,26 @@ class Program
     {
         //Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
 
+        Console.WriteLine("Here is a scripture to memorize: ");
+
         Console.WriteLine("Nephi 3:6-7 And behold, the third time they did understand the voice which they heard; and it said unto them: Behold my Beloved Son, in whom I am well pleased, in whom I have glorified my name—hear ye him.");
+
+        string reference = Console.ReadLine();
+        string text = Console.ReadLine();
+        Scripture scripture = new Scripture(reference, text);
+
+        Console.WriteLine("\nMemorize this scripture:");
+        Console.WriteLine(scripture.GetRenderedText());
+
+        
+
+
+
     }
 
-    
+
 
 }
+
+//https://www.churchofjesuschrist.org/study/scriptures/bofm?lang=eng
+
