bdbac9a940ef31cb7a1fcb643f9d822fb04bf183
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-20 05:06:10 -0500

    started coding Program.cs

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        //Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+    
+        Video video = new Video();
+
+        Comment comment = new Comment();
+
+        Console.WriteLine("Name of Youtube video and comments. ")
+    
     }
 }
