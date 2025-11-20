7cffc5c9b1f389d1aa8f480e65a5b4df6e8c08b7
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-20 04:57:31 -0500

    started coding Comment class file.

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.CompilerServices;
+
+public class Comment
+{
+    private string _name;
+    private string _comment;
+
+    public Comment (string name, string comment)
+    {
+        _name = name;
+        _comment = comment;
+    }
+
+    public string GetDisplayText()
+    {
+        return $"Name {_name} and comment {_comment}";
+
+    }
+}
+
+
+    
