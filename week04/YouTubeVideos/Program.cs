76ca7721ad31c9817d42bc9e66c388034f170c71
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-20 11:00:14 -0500

    Managed to get one video and corresponding comments to run.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -7,11 +7,16 @@ class Program
     {
         //Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
     
-        Video video = new Video();
+        Video videoOne = new Video("Battle of Culloden", "Inspiring History", 600);
 
-        Comment comment = new Comment();
+        Comment commentOne = new Comment("Loch Ness Monster", "Great video!");
+        string[] videos = {"'Loch Ness Monster'"};
+        string[] comments = {"'Wow'", "'Waste of time'", "'Old news'", "'Nothing new here'"};
 
-        Console.WriteLine("Name of Youtube video and comments. ")
+        Console.WriteLine($"Name of the video: {videos[0]}");
+
+        Console.WriteLine($"Comments: {string.Join(", ", comments.Take(4))}");
+        Console.WriteLine($"Number of comments: {comments.Length}");
     
     }
 }
