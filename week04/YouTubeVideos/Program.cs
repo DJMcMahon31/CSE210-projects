ab25afd78803c9216bf6ff5b0d7a2f211c55dc81
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-20 11:24:46 -0500

    added more video names,authors and length, as well as comments with person who wrote them.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
@@ -7,9 +8,14 @@ class Program
     {
         //Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
     
-        Video videoOne = new Video("Battle of Culloden", "Inspiring History", 600);
+        Video videoBattleOfCulloden = new Video("Battle of Culloden", "Brad Smith", 600);
+        Video videoLochNessMonster = new Video("Loch Ness Monster", "Travis MacDougall", 400);
+        Video videoIsleOfSkye = new Video("Isle of Skye", "Anna MacPhee", 1200);
 
-        Comment commentOne = new Comment("Loch Ness Monster", "Great video!");
+        Comment commentBattleOfCulloden = new Comment("Drew Sullivan", "Very informative");
+        Comment commentLochNessMonster = new Comment("Allan Munro", "Still don't know if the monster is real.");
+        Comment commentIsleOfSkye = new Comment("Queen Merida", "Who is in charge?");
+
         string[] videos = {"'Loch Ness Monster'"};
         string[] comments = {"'Wow'", "'Waste of time'", "'Old news'", "'Nothing new here'"};
 
