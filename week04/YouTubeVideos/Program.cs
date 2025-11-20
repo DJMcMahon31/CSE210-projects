e323df7a168da3f17b154b0a7c3278405d0db54e
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-20 13:59:22 -0500

    moved # of lines to just below name and anuthor and length of video.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -9,12 +9,13 @@ class Program
     {
         //Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
     
-        Video videoBattleOfCulloden = new Video("Battle of Culloden", "Brad Smith", 600);
-        Video videoLochNessMonster = new Video("Loch Ness Monster", "Travis MacDougall", 400);
-        Video videoIsleOfSkye = new Video("Isle of Skye", "Anna MacPhee", 1200);
+        Video videoBattleOfCulloden = new Video("'Battle of Culloden'", "Brad Smith", 600);
+        Video videoLochNessMonster = new Video("'Loch Ness Monster'", "Travis MacDougall", 400);
+        Video videoIsleOfSkye = new Video("'Isle of Skye'", "Anna MacPhee", 1200);
 
-        Comment commentBattleOfCulloden = new Comment("Drew Sullivan", "'Very informative', 'Tragic'");
-        Comment commentLochNessMonster = new Comment("Allan Munro", "'Still don't know if the monster is real.', 'Wow', 'Waste of time', 'Old news', 'Nothing new here'");
+        Comment commentBattleOfCulloden = new Comment("Drew Sullivan", "'Tragic'");
+    
+        Comment commentLochNessMonster = new Comment("Allan Munro", "'Still don't know if the monster is real.'");
         Comment commentIsleOfSkye = new Comment("Queen Merida", "Who is in charge?");
 
         string[] videoCulloden = {"'Battle of Culloden'"};  
@@ -27,27 +28,25 @@ class Program
 
      
         Console.WriteLine($"Video: {videoBattleOfCulloden.GetDisplayText()}"); //output is "Video: Battle of Culloden by Brad Smith (600 seconds)"
-        Console.WriteLine($"Comments: {commentBattleOfCulloden.GetDisplayText()}");
-
-    
         Console.WriteLine($"Number of comments: {commentCulloden.Length}"); //output is "Number of comments: 3"
+        Console.WriteLine($"Comments: {commentBattleOfCulloden.GetDisplayText()}");
         Console.WriteLine($"Comments: {string.Join(", ", commentCulloden.Take(4))}");  //output is Comments: 'Tragic', 'So long ago', 'Fascinating'"
         Console.WriteLine(); //adds a line between the separate videos.S
        
 
         Console.WriteLine($"Video: {videoLochNessMonster.GetDisplayText()}");
+        Console.WriteLine($"Number of comments: {commentMonster.Length}");
         Console.WriteLine($"Comments: {commentLochNessMonster.GetDisplayText()}");
 
      
-        Console.WriteLine($"Number of comments: {commentMonster.Length}");
         Console.WriteLine($"Comments: {string.Join(", ", commentMonster.Take(4))}");
         Console.WriteLine();
 
         Console.WriteLine($"Video: {videoIsleOfSkye.GetDisplayText()}");
+        Console.WriteLine($"Number of comments: {commentSkye.Length}");
         Console.WriteLine($"Comments: {commentIsleOfSkye.GetDisplayText()}");
 
         //Console.WriteLine("{videoSkye[0]}");
-        Console.WriteLine($"Number of comments: {commentSkye.Length}");
         Console.WriteLine($"Comments: {string.Join(", ", commentSkye.Take(4))}");
         Console.WriteLine();
 
