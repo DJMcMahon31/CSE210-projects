8db17603d9ec77f324dfb9b88fd278e21ec2c108
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-28 09:55:07 -0500

    updated placeholders in ListingActivity.cs

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -2,12 +2,14 @@ using System;
 
 public class ListingActivity : Activity
 {
-    private string _placeholderLA;
+    private int _count;
+    private string _prompts;
 
-    public ListingActivity(string placeHolder, string placeholderLA)
-        : base(placeHolder)
+    public ListingActivity(string name, string description, int duration, int count, string prompts)
+        : base(name, description, duration)
     {
-        _placeholderLA = placeholderLA;
+        _count = count;
+        _prompts = prompts;
     }
 
     //Console.WriteLine("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
