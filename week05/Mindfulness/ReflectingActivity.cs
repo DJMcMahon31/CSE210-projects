ab66fa5dc5471060ac168524419641abbec311ca
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-28 10:05:13 -0500

    changed file name of ReflectionActivity to ReflectingActivity

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -1,11 +1,11 @@
 using System;
 
-public class ReflectionActivity : Activity
+public class ReflectingActivity : Activity
 {
     private string _prompts;
     private string _questions;
 
-    public ReflectionActivity(string name, string description, int duration, string prompts, string questions)
+    public ReflectingActivity(string name, string description, int duration, string prompts, string questions)
         : base(name,description, duration)
     {
         _prompts = prompts;
