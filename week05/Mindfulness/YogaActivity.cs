bef2808acdbd12717f68c4476654af6af043fc7f
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-29 12:32:07 -0500

    Coded YogaActivity

--- week05/Mindfulness/YogaActivity.cs
+++ week05/Mindfulness/YogaActivity.cs
@@ -5,7 +5,7 @@ public class YogaActivity : Activity
     private List<string> _poses;
 
     public YogaActivity()
-        : base("Yoga", ""Welcome to the Yoga Activity. This activity will help you use common standing Yoga poses to help relieve tension.", 0)
+        : base("Yoga", "Welcome to the Yoga Activity. This activity will help you use common standing Yoga poses to help relieve tension.", 0)
     {
         _poses = new List<string>()
         {
@@ -19,29 +19,26 @@ public class YogaActivity : Activity
     {
         DisplayStartingMessage();
 
-        Console.Write("How long, in seconds, would you like for your session? ");
-        Duration = int.Parse(Console.ReadLine());
-        Console.WriteLine();
-
-        Console.WriteLine("Get ready...");
+        Console.WriteLine("Get ready for a yoga pose...");
         ShowSpinner(3);
         Console.WriteLine();
 
-        string _poses = GetRandomPoses();
-        Console.WriteLine($"-_-_- {_poses} -_-_-");
+        string pose = GetRandomPoses();
+        Console.WriteLine($"-_-_- Your pose is: {pose} -_-_-");
         Console.WriteLine("You may begin in:");
         ShowCountDown(5);
 
         Console.WriteLine();
 
+        Console.WriteLine($"Hold the {pose} for {Duration} seconds...");
+        ShowSpinner(Duration);
 
-
+        Console.WriteLine();
+        DisplayEndingMessage();
     }
 
     private string GetRandomPoses()
     {
-        return _poses[_rand.Next(_poses.)];
+        return _poses[_rand.Next(_poses.Count)];
     }
-
-
 }
