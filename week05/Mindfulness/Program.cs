aa8af4a0f3265b46b32c3b80d27ab98d3147b324
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-29 12:22:44 -0500

    Added new derived class to Program.cs list

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -16,7 +16,8 @@ class Program
             Console.WriteLine("1. Breathing Activity");
             Console.WriteLine("2. Reflecting Activity");
             Console.WriteLine("3. Listing Activiy");
-            Console.WriteLine("4. Quit");
+            Console.WriteLine("4. Yoga");
+            Console.WriteLine("5. Quit");
             Console.Write("Select a choice from the menu:");
 
             string choice = Console.ReadLine();
@@ -39,6 +40,11 @@ class Program
                     break;
 
                 case "4":
+                    YogaActivity yoga = new YogaActivity();
+                    yoga.Run();
+                    break;
+
+                case "5":
                     running = false;
                     Console.WriteLine("Goodbye!");
                     break;
--- week05/Mindfulness/YogaActivity.cs
+++ week05/Mindfulness/YogaActivity.cs
@@ -2,11 +2,46 @@ using System;
 
 public class YogaActivity : Activity
 {
-    private List<string> _poses = new List<string>
+    private List<string> _poses;
 
-    public YogaActivity(string placeHolder, string placeholderAA)
-        : base(placeHolder)
+    public YogaActivity()
+        : base("Yoga", ""Welcome to the Yoga Activity. This activity will help you use common standing Yoga poses to help relieve tension.", 0)
     {
-        _placeholderAA = placeholderAA;
+        _poses = new List<string>()
+        {
+            "Mountain Pose",
+            "Tree Pose",
+            "Extended Triangle Pose",
+            "Chair Pose"
+        };
     }
+    public override void Run()
+    {
+        DisplayStartingMessage();
+
+        Console.Write("How long, in seconds, would you like for your session? ");
+        Duration = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        Console.WriteLine("Get ready...");
+        ShowSpinner(3);
+        Console.WriteLine();
+
+        string _poses = GetRandomPoses();
+        Console.WriteLine($"-_-_- {_poses} -_-_-");
+        Console.WriteLine("You may begin in:");
+        ShowCountDown(5);
+
+        Console.WriteLine();
+
+
+
+    }
+
+    private string GetRandomPoses()
+    {
+        return _poses[_rand.Next(_poses.)];
+    }
+
+
 }
