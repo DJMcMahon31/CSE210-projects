3a496fb53cf4e56a2179f08cb1fe221b3e78c29a
Dianne McMahon <dn2mcmahon@gmail.com>
2025-11-29 11:19:19 -0500

    coded Program.cs

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,7 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        //Console.WriteLine("Hello World! This is the Mindfulness Project.");
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.Clear();
+            Console.WriteLine("Mindfulness Activities:");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting Activity");
+            Console.WriteLine("3. Listing Activiy");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the menu:");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.Run();
+                    break;
+
+                case "2":
+                    ReflectingActivity reflecting = new ReflectingActivity();
+                    reflecting.Run();
+                    break;
+
+                case "3":
+                    ListingActivity listing = new ListingActivity();
+                    listing.Run();
+                    break;
+
+                case "4":
+                    running = false;
+                    Console.WriteLine("Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("\nInvalid option, please try again.");
+                    Thread.Sleep(1500);
+                    break;
+            }
+        }
     }
 
     /*
