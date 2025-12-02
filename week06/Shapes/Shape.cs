9c86fe6b3057809912cc9148356fe8ed2e06c69f
Dianne McMahon <dn2mcmahon@gmail.com>
2025-12-01 21:26:56 -0500

    added cooments to Shape.cs

--- week06/Shapes/Shape.cs
+++ week06/Shapes/Shape.cs
@@ -2,8 +2,14 @@ using System;
 
 public abstract class Shape
 {    
-    public string Colour { get; set; }
+    public string Color { get; set; }  //base class does not need a constructor in this case. The derived classes handle the color assignment
+    //a base constructor is only needed when the base class needs to force certain data at creation, or the derived classes rely on shared initialization logic
+    //Color can be set: after the object is created; inside the derived class constructor; by the user.
 
+    //e.g. 'private string _color' is not needed because the auto-properties have already stored this internally
+    //e.g. 'public Shape(string color' is not needed because derived classes already receive and set the color
+    //e.g. '_color = color' is not needed because the Auto-property setter(Color = color) already handles the assignment
+    
     public abstract double GetArea();
 
 }
